7c60291944b5350d02b6cf439de186ee98589785
anik5001 <aniksarker047@gmail.com>
2025-11-10 00:25:26 +0600

    update model api using put complete

--- index.js
+++ index.js
@@ -52,6 +52,18 @@ async function run() {
       res.send(result);
     });
 
+    // update model api
+    app.put("/update-model/:id", async (req, res) => {
+      const id = req.params.id;
+      const data = req.body;
+      const query = { _id: new ObjectId(id) };
+      const update = {
+        $set: data,
+      };
+      const result = await ModelCollection.updateOne(query, update);
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
