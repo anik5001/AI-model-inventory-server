4e8281da83925a6af1f9a4001fd89db3be8b38a6
anik5001 <aniksarker047@gmail.com>
2025-11-10 01:46:54 +0600

    my models api create complete

--- index.js
+++ index.js
@@ -64,6 +64,14 @@ async function run() {
       res.send(result);
     });
 
+    // my models api
+
+    app.get("/my-models", async (req, res) => {
+      const email = req.query.email;
+      const result = await ModelCollection.find({ createdBy: email }).toArray();
+      res.send(result);
+    });
+
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
     console.log(
